f6e9c2c88d806d827787a9a8577ca1be9d19cae3
Darkdadaah <darkdadaah@yahoo.fr>
2017-05-23 11:58:11 +0100

    Add tags to sections of WiktArticleCommon

--- pywikibot/wiktionary_page.py
+++ pywikibot/wiktionary_page.py
@@ -10,9 +10,9 @@ import sys
 from warnings import warn
 from wiki_page import WikiArticle
 
-class WiktArticle(WikiArticle):
+class WiktArticleCommon(WikiArticle):
     """
-    WiktArticle: Wiktionary article object.
+    WiktArticleCommon: Wiktionary article object.
     """
     
     def __init__(self, title, text, lang):
@@ -25,4 +25,28 @@ class WiktArticle(WikiArticle):
         @type text: Str
         """
         WikiArticle.__init__(self, title, text, lang);
-
+        
+    def tag_sections(self):
+        """
+        Analyze the section titles to identify them
+        For all Wiktionaries we will make the assumption that level 2 titles are 
+        always language sections
+        Identifying other types (word types in particular will have to be done
+        with language specific analyzes)
+        The type of language section is saved as a tag
+        """
+        # First, get all the language sections
+        top = self.top_section
+        
+        # Then, look at the title of every section and change its tag accordingly
+        self.rec_tag_sections(top)
+       
+    def rec_tag_sections(self, section):
+        if section.level == 2:
+            section.tag = 'lang'
+        # This is a very simple version because it can only be used generically
+        # Each language will need to reimplement this
+            
+        for sub_section in section.sub_sections:
+            self.rec_tag_sections(sub_section)
+    
