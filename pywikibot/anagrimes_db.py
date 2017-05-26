00f732f98bf47d848bd473831f8e372789c9f438
Darkdadaah <darkdadaah@yahoo.fr>
2017-05-26 17:20:41 +0100

    Initial support for Mysql DB

--- pywikibot/anagrimes_db.py
+++ pywikibot/anagrimes_db.py
@@ -11,58 +11,71 @@ from warnings import warn
 
 from sqlalchemy import create_engine
 from sqlalchemy import exc
-from sqlalchemy import Column, DateTime, String, Integer, Boolean, ForeignKey, func
+from sqlalchemy import Column, Index, DateTime, String, UnicodeText, Integer, Boolean, ForeignKey, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Article(Base):
+class BaseEntity(object):
+    __table_args__ = {  
+            'mysql_engine': 'InnoDB',
+            'mysql_default charset': 'utf8',
+            'mysql_collate': 'utf8_bin',
+            }
+
+class Article(Base, BaseEntity):
     __tablename__ = 'article'
     id    = Column(Integer, primary_key=True)
-    title = Column(String, unique=True)
+    title = Column(String(256), index=True)
     #last_update = Column(DateTime, default=func.now())
     errors = relationship("Error", back_populates='article', cascade="all, delete, delete-orphan")
     lexemes = relationship("Lexeme", back_populates='article', cascade="all, delete, delete-orphan")
 
-class Lexeme(Base):
+class Lexeme(Base, BaseEntity):
     __tablename__ = 'lexeme'
     id          = Column(Integer, primary_key=True)
-    article_id  = Column(Integer, ForeignKey('article.id'))
+    article_id  = Column(Integer, ForeignKey('article.id'), index=True)
     article     = relationship("Article", back_populates="lexemes")
-    lang        = Column(String)
-    type        = Column(String)
+    lang        = Column(String(32), index=True)
+    type        = Column(String(64), index=True)
     num         = Column(Integer)
-    flex        = Column(Boolean)
-    loc         = Column(Boolean)
+    flex        = Column(Boolean, index=True)
+    loc         = Column(Boolean, index=True)
 
-class Error(Base):
+class Error(Base, BaseEntity):
     __tablename__ = 'error'
     id          = Column(Integer, primary_key=True)
-    article_id  = Column(Integer, ForeignKey('article.id'))
+    article_id  = Column(Integer, ForeignKey('article.id'), index=True)
     article     = relationship("Article", back_populates="errors")
-    description = Column(String)
-    error_type  = Column(String)
+    description = Column(UnicodeText)
+    error_type  = Column(UnicodeText)
 
-class Meta(Base):
+class Meta(Base, BaseEntity):
     __tablename__ = 'meta'
     id    = Column(Integer, primary_key=True)
-    key   = Column(String, unique=True)
-    value = Column(String)
+    key   = Column(UnicodeText)
+    value = Column(UnicodeText)
     timestamp   = Column(DateTime, default=func.now())
 
 class AnagrimesDB():
 
-    def __init__(self, path):
+    def __init__(self, db_con):
         """
         Instantiate an anagrimes DB (SQLite for now).
         
         @param path: path to the SQL db file
         @type path: Str
         """
-        self.path = path
         self.from_zero = True
+        self.db_con = db_con
+        if 'sqlite_path' in db_con:
+            self.db_type = 'sqlite'
+        elif 'mysql_db' in db_con:
+            self.db_type = 'mysql'
+        else:
+            raise Exception("No proper DB connection provided")
         
     def commit(self):
         self.session.commmit()
@@ -73,11 +86,27 @@ class AnagrimesDB():
         if the tables do not exist
         Also create the SQLite file if needed
         """
-        engine  = create_engine('sqlite:///' + self.path)
+        #try:
+        db_con = self.db_con
+        if self.db_type == 'sqlite':
+            engine  = create_engine('sqlite:///' + db_con['sqlite_path'])
+        elif self.db_type == 'mysql':
+            engine = create_engine("mysql+pymysql://%s:%s@%s/%s?host=%s?port=%s?charset=%s" % 
+                        (db_con['mysql_user'],
+                        db_con['mysql_pass'],
+                        db_con['mysql_host'],
+                        db_con['mysql_db'],
+                        db_con['mysql_host'],
+                        db_con['mysql_port'],
+                        'utf8'
+                        )
+                    )
         session = sessionmaker()
         session.configure(bind=engine)
         Base.metadata.create_all(engine)
         db = session()
+        #except Exception as e:
+        #    raise Exception("Could not connect to the db: %s" % unicode(e))
         
         # Store the session and if the db was empty
         self.session = db
@@ -90,7 +119,7 @@ class AnagrimesDB():
         @param lang: wikimedia chapter code
         @type lang: Str
         """
-        meta_lang = Meta(key='language', value=lang)
+        meta_lang = Meta(key=u'language', value=lang)
         db = self.session
         if self.from_zero:
             db.add(meta_lang)
--- scripts/wikt_reader.py
+++ scripts/wikt_reader.py
@@ -7,6 +7,7 @@ Print a list of Wiktionary pages.
 
 &params;
 """
+from __future__ import unicode_literals
 
 import os
 import re
@@ -22,19 +23,19 @@ from pywikibot.xmlreader import XmlDump
 from pywikibot.wiktionary_page_fr import WiktArticle
 from pywikibot.anagrimes_db import AnagrimesDB
 
-def read_dump(dump_path, sqlite_path):
+def read_dump(dump_path, db_con):
     # Parse the data
     xml = XMLDumpOldPageGenerator(dump_path)
     dump = XmlDump(dump_path)
     dump.parse_siteinfo()
-    language = dump.siteinfo['lang']
+    language = unicode(dump.siteinfo['lang'])
     if language == 'frwiktionary':
         from pywikibot.wiktionary_page_fr import WiktArticle
     else:
         raise ValueError("Language %s is not supported" % language)
     
-    # Then create the SQLite DB
-    db = AnagrimesDB(sqlite_path)
+    # Then create the connection to the DB
+    db = AnagrimesDB(db_con)
     db.create_db()
     db.define_language(language)
     
@@ -64,8 +65,8 @@ def page_to_db(db, page, language):
         text.strip()
     
         art = WiktArticle(
-                title = page.title,
-                text  = page.text,
+                title = unicode(page.title),
+                text  = unicode(page.text),
                 lang  = language
                 )
         
@@ -88,16 +89,26 @@ def main(*args):
     """
     local_args = pywikibot.handle_args(args)
     dump = None
-    sqlite = None
+    db_con = {}
     
     for arg in local_args:
         if arg.startswith('-dump:'):
             dump = arg[len('-dump:'):]
         if arg.startswith('-sqlite:'):
-            sqlite = arg[len('-sqlite:'):]
+            db_con['sqlite_path'] = arg[len('-sqlite:'):]
+        if arg.startswith('-mysql_user:'):
+            db_con['mysql_user'] = arg[len('-mysql_user:'):]
+        if arg.startswith('-mysql_pass:'):
+            db_con['mysql_pass'] = arg[len('-mysql_pass:'):]
+        if arg.startswith('-mysql_host:'):
+            db_con['mysql_host'] = arg[len('-mysql_host:'):]
+        if arg.startswith('-mysql_port:'):
+            db_con['mysql_port'] = arg[len('-mysql_port:'):]
+        if arg.startswith('-mysql_db:'):
+            db_con['mysql_db'] = arg[len('-mysql_db:'):]
     
-    if dump and sqlite:
-        read_dump(dump, sqlite)
+    if dump and db_con:
+        read_dump(dump, db_con)
     else:
         pywikibot.warning(u'Need -dump and -sqlite to proceed')
 
